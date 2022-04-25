c34b0575d76dd81c9ff96655f66d46c7624edd7d
Leo Loureiro <leolou.90@gmail.com>
2022-04-24 19:02:15 -0700

    add showScroll to change opacity to html elements using fade class

--- root/index.js
+++ root/index.js
@@ -1,60 +1,33 @@
-const hamburger = document.getElementById("check-btn");
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-const links = Array.from(document.getElementsByClassName('link'));
-const ul = document.getElementById("link-list");
-const section = document.querySelectorAll('section');
-const navList = document.querySelectorAll('header nav ul li');
-const hero = document.getElementById('home');
-let clicked = false
+const fade = document.querySelectorAll(".fade");
 
 
-const show = () =>{
-    
+/* Function to change hero image */
+const changeHero = () =>{
+    let counter = 0;
+    if(counter < carrousselImages.length -1){
+        document.getElementById('home').style.backgroundImage = "url("+carrousselImages[counter] +")";
+         counter++;
+         console.log(carrousselImages[counter])
+     }else{
+         counter = 0;
+    }
 }
 
-setInterval(
-    () => {show()}, 
-    4000);
+changeHero(carrousselImages);
+
 
 /*
-    Make navbar appears and disappears clicking hamburger menu
-*/
-hamburger.addEventListener('click', () =>{
-    if(clicked === false){
-        ul.style.left = "0"
-        clicked = true;
-    }else{
-        ul.style.left = "-100%";
-        clicked = false;
-       
+Function to fade-in page's components
+*/ 
+const showScroll = () =>{
+    let scrollTop = document.documentElement.scrollTop;
+    for(let i = 0; i < fade.length; i++){
+        let height = fade[i].offsetTop;
+        if(height - 500 < scrollTop){
+            fade[i].style.opacity = 1;
+        }
     }
-})
+}
 
-/*
-    Make navbar appears and disappears clicking an anchor
-*/
-links.forEach(link => {
-        link.addEventListener('click', () =>{
-            ul.style.left = "-100%"
-            clicked = false;
-        })
-})
-
-window.addEventListener('scroll', () =>{
-    let current = "";
-    section.forEach(section =>{
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-
-        if(window.scrollY > sectionTop){
-            current = section.getAttribute('id');
-        }
-    })
-    navList.forEach(li =>{
-        li.classList.remove('active');
-        if(li.classList.contains(current)){
-            // li.classList.add('active');
-            console.log(li)
-        }
-    })
-})
+window.addEventListener('scroll', showScroll);
+window.addEventListener('load', changeHero);
